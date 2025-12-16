85362e9b8ae1a69759103d458e60b61855f133f2
NBM <notbigmuzzy@gmail.com>
2025-12-17 00:02:24 +0100

    setup gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/linkwalk/',
   build: {
     // Three.js apps commonly exceed the default 500kb warning.
     // This does not affect runtime size; it only quiets the warning.
